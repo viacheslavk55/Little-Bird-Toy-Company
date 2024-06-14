3ae8331ee1127a1e09bd6db0f3cb78df97fe9e1a
viacheslavk55 <525vkostenko@frhsd.com>
2024-06-14 08:46:29 -0400

    final project

--- script.js
+++ script.js
@@ -41,7 +41,7 @@ const products = [
         price: "$19.99",
         onClickFunction: "duck()"
     }
-];
+]
 function goBack() {
     window.location.href = "store.html"
 }
@@ -59,65 +59,65 @@ function generateProductHTML(product) {
                 </div>
             </div>
         </div>
-    `;
+    `
 }
 
 function generateProducts(productsToShow) {
     const productContainer = document.getElementById("productContainer");
-    productContainer.innerHTML = ""; // Clear previous content
+    productContainer.innerHTML = "" // Clearing any previous content
 
     productsToShow.forEach(product => {
-        productContainer.innerHTML += generateProductHTML(product);
-    });
+        productContainer.innerHTML += generateProductHTML(product)
+    })
 }
 
 function filterProducts(searchText) {
     const filteredProducts = products.filter(product => {
-        const name = product.name.toLowerCase();
-        return name.includes(searchText.toLowerCase());
-    });
+        const name = product.name.toLowerCase()
+        return name.includes(searchText.toLowerCase())
+    })
 
-    generateProducts(filteredProducts);
+    generateProducts(filteredProducts)
 }
 
 function init() {
-    const searchInput = document.getElementById('searchInput');
+    const searchInput = document.getElementById('searchInput')
 
     searchInput.addEventListener('input', function() {
-        const searchText = this.value.trim();
+        const searchText = this.value.trim()
 
         if (searchText === '') {
-            generateProducts(products); // Show all products if search bar is empty
+            generateProducts(products) // Show all products if search bar is empty
         } else {
-            filterProducts(searchText); // Filter products based on search text
+            filterProducts(searchText) // Filter products based on search text
         }
-    });
+    })
 
-    generateProducts(products); // Show all products initially
+    generateProducts(products) // Show all products initially
 }
 
-init();
+init()
 
 function blocks() {
-    window.location.href = "blocks.html";
+    window.location.href = "blocks.html"
 }
 
 function boat() {
-    window.location.href = "boat.html";
+    window.location.href = "boat.html"
 }
 
 function car() {
-    window.location.href = "car.html";
+    window.location.href = "car.html"
 }
 
 function duck() {
-    window.location.href = "duck.html";
+    window.location.href = "duck.html"
 }
 
 function train() {
-    window.location.href = "train.html";
+    window.location.href = "train.html"
 }
 
 function plane() {
-    window.location.href = "plane.html";
+    window.location.href = "plane.html"
 }
